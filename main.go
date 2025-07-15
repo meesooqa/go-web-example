45eaecc79e3b1c083f9778c2cbdb49658a77f0c4
meesooqa <meesooqa@hotmail.com>
2025-07-15 17:36:30 +0300

    added code to run server

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"html/template"
 	"log"
-	"net/http"
+
+	"github.com/meesooqa/go-web-example/config"
+	"github.com/meesooqa/go-web-example/logging"
+	"github.com/meesooqa/go-web-example/server"
+	"github.com/meesooqa/go-web-example/server/handlers"
+	"github.com/meesooqa/go-web-example/server/middlewares"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("templates/index.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	cfg, err := config.Load("etc/config.yml")
+	if err != nil {
+		log.Fatalf("loading config: %v", err)
+	}
+	logger, closer := logging.New(cfg.Log)
+	if closer != nil {
+		defer func() { _ = closer.Close() }()
+	}
 
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	hh := []server.Handler{
+		handlers.NewStatic(logger, cfg.Handler),
+		handlers.NewIndex(logger, cfg.Handler),
+	}
+	mw := []server.Middleware{
+		middlewares.NewLogging(logger),
+	}
+	s := server.New(cfg.Server, hh, mw)
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	port := ":8080"
-	log.Printf("Сервер запущен на http://localhost%s", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Printf("Сервер запущен на http://%s:%d", cfg.Server.Host(), cfg.Server.Port())
+	log.Fatal(s.Run())
 }
