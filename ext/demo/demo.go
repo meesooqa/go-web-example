2f7661f2f5f5adc3c7bd78c5f031a1a023d61d9b
meesooqa <meesooqa@hotmail.com>
2025-07-17 20:12:05 +0300

    feat: register dynamic script in initialization and remove inline script from HTML

--- ext/demo/demo.go
+++ ext/demo/demo.go
@@ -7,4 +7,5 @@ const code = "demo"
 func init() {
 	theme.RegisterMenu(mainMenu)
 	theme.RegisterCSS("<link rel=\"stylesheet\" href=\"/ext/demo/static/styles/styles.css\">")
+	theme.RegisterScript("<script type=\"module\" src=\"/ext/demo/static/scripts/index.js\"></script>")
 }
