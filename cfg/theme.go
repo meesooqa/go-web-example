715c569025c56e9da3435d3e8679b99cced5231b
meesooqa <meesooqa@hotmail.com>
2025-07-18 14:14:30 +0300

    feat: add ext_dir configuration to theme and update theme struct

--- cfg/theme.go
+++ cfg/theme.go
@@ -4,6 +4,7 @@ package cfg
 type Theme struct {
 	RawThemesDir string `yaml:"themes_dir"`
 	RawTheme     string `yaml:"theme"`
+	RawExtDir    string `yaml:"ext_dir"`
 }
 
 func (cfg *Theme) ThemesDir() string {
@@ -15,6 +16,5 @@ func (cfg *Theme) Theme() string {
 }
 
 func (cfg *Theme) ExtDir() string {
-	// TODO config ext_dir
-	return "ext"
+	return cfg.RawExtDir
 }
